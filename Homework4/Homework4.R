c252fb1fdbc03e22507a10330d390b5d1ff4a634
David Mo <qmo@ucdavis.edu>
2019-11-17 19:44:48 -0800

    11172019

--- Homework4/Homework4.R
+++ Homework4/Homework4.R
@@ -71,75 +71,29 @@ step.model_1 <- stepAIC(reg_1, direction = "both",
                         trace = FALSE)
 summary(step.model_1)
 
-
-########################################ModleTwo#############################################
-##add dummy variable
 data_dummy_add_2 <- data_dummy %>%
-  dplyr::mutate(
-    Jan_week1_price = month_Jan*week_1*Price,
-    Jan_week2_price = month_Jan*week_2*Price,
-    Jan_week3_price = month_Jan*week_3*Price,
-    Jan_week4_price = month_Jan*week_4*Price,
-    Jan_week5_price = month_Jan*week_5*Price,
-    Feb_week1_price = month_Feb*week_1*Price,
-    Feb_week2_price = month_Feb*week_2*Price,
-    Feb_week3_price = month_Feb*week_3*Price,
-    Feb_week4_price = month_Feb*week_4*Price,
-    Feb_week5_price = month_Feb*week_5*Price,
-    Mar_week1_price = month_Mar*week_1*Price,
-    Mar_week2_price = month_Mar*week_2*Price,
-    Mar_week3_price = month_Mar*week_3*Price,
-    Mar_week4_price = month_Mar*week_4*Price,
-    Mar_week5_price = month_Mar*week_5*Price,
-    Apr_week1_price = month_Apr*week_1*Price,
-    Apr_week2_price = month_Apr*week_2*Price,
-    Apr_week3_price = month_Apr*week_3*Price,
-    Apr_week4_price = month_Apr*week_4*Price,
-    Apr_week5_price = month_Apr*week_5*Price,
-    May_week1_price = month_May*week_1*Price,
-    May_week2_price = month_May*week_2*Price,
-    May_week3_price = month_May*week_3*Price,
-    May_week4_price = month_May*week_4*Price,
-    May_week5_price = month_May*week_5*Price,
-    Jun_week1_price = month_Jun*week_1*Price,
-    Jun_week2_price = month_Jun*week_2*Price,
-    Jun_week3_price = month_Jun*week_3*Price,
-    Jun_week4_price = month_Jun*week_4*Price,
-    Jun_week5_price = month_Jun*week_5*Price,
-    Jul_week1_price = month_Jul*week_1*Price,
-    Jul_week2_price = month_Jul*week_2*Price,
-    Jul_week3_price = month_Jul*week_3*Price,
-    Jul_week4_price = month_Jul*week_4*Price,
-    Jul_week5_price = month_Jul*week_5*Price,
-    Aug_week1_price = month_Aug*week_1*Price,
-    Aug_week2_price = month_Aug*week_2*Price,
-    Aug_week3_price = month_Aug*week_3*Price,
-    Aug_week4_price = month_Aug*week_4*Price,
-    Aug_week5_price = month_Aug*week_5*Price,
-    Sep_week1_price = month_Sep*week_1*Price,
-    Sep_week2_price = month_Sep*week_2*Price,
-    Sep_week3_price = month_Sep*week_3*Price,
-    Sep_week4_price = month_Sep*week_4*Price,
-    Sep_week5_price = month_Sep*week_5*Price,
-    Oct_week1_price = month_Oct*week_1*Price,
-    Oct_week2_price = month_Oct*week_2*Price,
-    Oct_week3_price = month_Oct*week_3*Price,
-    Oct_week4_price = month_Oct*week_4*Price,
-    Oct_week5_price = month_Oct*week_5*Price,
-    Nov_week1_price = month_Nov*week_1*Price,
-    Nov_week2_price = month_Nov*week_2*Price,
-    Nov_week3_price = month_Nov*week_3*Price,
-    Nov_week4_price = month_Nov*week_4*Price,
-    Nov_week5_price = month_Nov*week_5*Price,
-    Dec_week1_price = month_Dec*week_1*Price,
-    Dec_week2_price = month_Dec*week_2*Price,
-    Dec_week3_price = month_Dec*week_3*Price,
-    Dec_week4_price = month_Dec*week_4*Price,
-    Dec_week5_price = month_Dec*week_5*Price
-  )
+  dplyr::mutate(price_x_week5 = Price*week_5,
+                price_x_week4 = Price*week_4,
+                price_x_week3 = Price*week_3,
+                price_x_week2 = Price*week_2,
+                price_x_week1 = Price*week_1
+               
+                )
 
-reg_2 <- lm(Seats.Sold~., data = data_dummy_add_2)
+reg_2 <- lm(Seats.Sold~price_x_week5+
+              price_x_week4+
+              price_x_week3+
+              price_x_week2+
+              price_x_week1+
+              week_5+
+              week_4+
+              week_3+
+              week_2+
+              week_1,
+              data = data_dummy_add_2)
+summary(reg_2)
 
-step.model_2 <- stepAIC(reg_2, direction = "both", 
-                        trace = FALSE)
-summary(step.model_2)
+seat_sold <- function(p5,p4,p3,p2,p1) (46.748489+(-0.214206*p5)+70.483956)+(46.748489+(-0.216287*p4)+
+                                    85.130350)+(46.748489+(-0.193156*p3)+70.408310)+(46.748489+(-0.164923*p2)+48.274968)+(46.748489+(-0.085006*p1))
+optim(c(366,373,404,417,374), fn=seat_sold, method = "BFGS")
+
